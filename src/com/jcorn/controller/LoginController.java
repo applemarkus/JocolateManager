0c7e5bcc20aefa54a980d5b00b5b5288fb7384d1
Markus Petritz <petritzdesigns@gmail.com>
2015-02-09 14:57:01 +0100

    Login added

---
+++ src/com/jcorn/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.jcorn.controller;
+
+/**
+ * JocolateManager
+ *
+ * @author Markus Petritz
+ * @version 1.0.0
+ * @see http://petritzdesigns.com
+ */
+public class LoginController {
+
+    public String login(String username, String password) throws Exception {
+        return "";
+    }
+}
