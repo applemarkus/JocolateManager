dff03246b9284796a090705d4da2affc3fe1994a
Markus Petritz <petritzdesigns@gmail.com>
2015-02-09 14:40:31 +0100

    WebApiLinks added

---
+++ src/com/jcorn/helper/WebApiLinks.java
@@ -0,0 +1,18 @@
+package com.jcorn.helper;
+
+/**
+ * JocolateManager
+ * 
+ * @author Markus Petritz
+ * @version 1.0.0
+ * @see http://petritzdesigns.com
+ */
+public class WebApiLinks {
+    
+    public static final String api = "http://jocolate.petritzdesigns.at/api/webapi.php";
+    public static final String loginAction = "?action=login";
+    
+    public static String getLoginApiLink(String username, String password) {
+        return api + loginAction + "&username=" + username + "&password" + password;
+    }
+}
