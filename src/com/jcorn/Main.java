2da9c244b0b52fb94f4d16d298f70b54afb9bc50
Markus Petritz <petritzdesigns@gmail.com>
2015-02-09 10:33:30 +0100

    Main Class added

---
+++ src/com/jcorn/Main.java
@@ -0,0 +1,16 @@
+package com.jcorn;
+
+/**
+ * JocolateManager
+ * 
+ * @author Markus Petritz
+ * @version 1.0.0
+ * @see http://petritzdesigns.com
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
