79a5231db6210d0032ed37f26d6fd832bdfbf15d
Markus Petritz <petritzdesigns@gmail.com>
2015-02-10 10:31:53 +0100

    Price changed

--- src/com/jcorn/controller/JocolateController.java
+++ src/com/jcorn/controller/JocolateController.java
@@ -15,25 +15,41 @@ public class JocolateController {
     public static Double calculatePrice(JocolateModel model) {
         /*
         Type:
-            * milk chocolate        0,00
-            * white chocolate       0,05
-            * dark chocolate        0,10
-            * pure cocoa chocolate  0,20
+            * milk chocolate        0,00 t1
+            * white chocolate       0,05 t2
+            * dark chocolate        0,10 t3
+            * pure cocoa chocolate  0,20 t4
         
         Size:
-            * ultra-big         8,99
-            * big               7,99
-            * middle            6,99
-            * small             4,99
-            * slim              2,99
+            * ultra-big         8,99 s1
+            * big               7,99 s2
+            * middle            6,99 s3
+            * small             4,99 s4
+            * slim              2,99 s5
         
         Logo:
-            * Default           0,00
-            * PetritzDesigns    0,50
-            * jCorn             0,30
-            * Happy Birthday    0,80
-            * Text              1,00
+            * Default           0,00 l1
+            * PetritzDesigns    0,50 l2
+            * jCorn             0,30 l3
+            * Happy Birthday    0,80 l4
+            * Text              1,00 l5
         */
+        double t1 = 0.00;
+        double t2 = 0.05;
+        double t3 = 0.10;
+        double t4 = 0.20;
+        
+        double s1 = 8.99;
+        double s2 = 7.99;
+        double s3 = 6.99;
+        double s4 = 4.99;
+        double s5 = 2.99;
+        
+        double l1 = 0.00;
+        double l2 = 0.50;
+        double l3 = 0.30;
+        double l4 = 0.80;
+        double l5 = 1.00;
         
         String type = model.getType();
         String size = model.getSize();
@@ -41,7 +57,26 @@ public class JocolateController {
        
         Double price = 0.0;
         
-        return model.getAmount() * 10 + new Random().nextDouble();
+        switch(type) {
+            case "milk chocolate":
+                price += t1;
+                break;
+            case "white chocolate":
+                price += t2;
+                break;
+            case "dark chocolate":
+                price += t3;
+                break;
+            case "pure cocoa chocolate":
+                price += t4;
+                break;
+        }
+        
+        switch(size) {
+            
+        }
+        
+        return model.getAmount() * price;
     }
 
 }
