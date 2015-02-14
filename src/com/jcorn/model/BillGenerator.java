995eb04bd26caa75a6fcb8f38e611958ecf044de
petritzdesigns <petritzdesigns@gmail.com>
2015-02-14 20:03:53 +0100

    Bill: Shopping Items added

--- src/com/jcorn/model/BillGenerator.java
+++ src/com/jcorn/model/BillGenerator.java
@@ -1,12 +1,11 @@
 package com.jcorn.model;
 
-import com.jcorn.helper.Crypt;
 import com.jcorn.helper.FileHelper;
 import com.jcorn.helper.HTTPRequester;
 import com.jcorn.helper.JM;
 import com.jcorn.helper.Settings;
 import com.jcorn.helper.WebApiLinks;
-import java.io.File;
+import java.io.PrintWriter;
 import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -53,6 +52,8 @@ public class BillGenerator {
 
         Element shoppingCart = doc.createElement("ShoppingCart");
         rootElement.appendChild(shoppingCart);
+        
+        appendShoppingCart(doc, shoppingCart);
 
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
@@ -66,10 +67,47 @@ public class BillGenerator {
     public void sendToWebServer() throws Exception {
         String billPlain = FileHelper.readAllFrom(Settings.getTempXmlFile());
         String response = HTTPRequester.getContentFromUrlWithPost(WebApiLinks.postBill, WebApiLinks.getPostBillParam(billPlain));
-        JM.debug(response);
+        parseXml(response);
+    }
+    
+    private void parseXml(String xmlData) throws Exception {
+        FileHelper.clearFile(Settings.getTempXmlFile());
+        
+        String login = "";
+        String status = "";
+        String error = "";
+        
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        
+        //temp file
+        try (
+            PrintWriter out = new PrintWriter(Settings.getTempXmlFile())) {
+            out.print(xmlData);
+            out.flush();
+        }
+        
+        Document document = builder.parse(Settings.getTempXmlFile());
+        status = document.getDocumentElement().getElementsByTagName("bill").item(0).getChildNodes().item(0).getNodeValue();
+        error = document.getDocumentElement().getElementsByTagName("error").item(0).getChildNodes().item(0).getNodeValue();
+        login = document.getDocumentElement().getElementsByTagName("login").item(0).getChildNodes().item(0).getNodeValue();
+        
+        JM.debug("status: "+status);
+        JM.debug("error: "+error);
+        JM.debug("login "+login);
+        
+        if(!error.equals("none")) {
+            throw new Exception(error);
+        }
+        
+        if(status.equals("failure")) {
+            throw new Exception("Could not send Bill to Webserver. Please contact your webmaster!");
+        }
+        
+        FileHelper.clearFile(Settings.getTempXmlFile());
     }
 
-    private void appendElements(Document doc, Element elem) {
+    private void appendElements(Document doc, Element elem) throws Exception {
         Element name = doc.createElement("name");
         name.appendChild(doc.createTextNode(details.getName()));
         elem.appendChild(name);
@@ -121,5 +159,36 @@ public class BillGenerator {
         Element date = doc.createElement("date");
         date.appendChild(doc.createTextNode(details.getDate()));
         elem.appendChild(date);
+    }
+    
+    private void appendShoppingCart(Document doc, Element elem) throws Exception {
+        for (ShoppingCartItem item : list) {
+            Element shoppingItem = doc.createElement("ShoppingCartItem");
+            elem.appendChild(shoppingItem);
+            
+            Element name = doc.createElement("name");
+            name.appendChild(doc.createTextNode(item.getName()));
+            shoppingItem.appendChild(name);
+            
+            Element type = doc.createElement("type");
+            type.appendChild(doc.createTextNode(item.getType()));
+            shoppingItem.appendChild(type);
+            
+            Element size = doc.createElement("size");
+            size.appendChild(doc.createTextNode(item.getSize()));
+            shoppingItem.appendChild(size);
+            
+            Element logo = doc.createElement("logo");
+            logo.appendChild(doc.createTextNode(item.getLogo()));
+            shoppingItem.appendChild(logo);
+            
+            Element amount = doc.createElement("amount");
+            amount.appendChild(doc.createTextNode(item.getAmount() + ""));
+            shoppingItem.appendChild(amount);
+            
+            Element price = doc.createElement("price");
+            price.appendChild(doc.createTextNode(String.format("%4.2f", item.getPrice())));
+            shoppingItem.appendChild(price);
+        }
     }
 }
