29845a1648f210b36312c7926f4f5c1800da14b4
petritzdesigns <petritzdesigns@gmail.com>
2015-02-14 10:51:29 +0100

    API optimzied

--- web/application/controllers/Api.php
+++ web/application/controllers/Api.php
@@ -2,6 +2,11 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+define("LOGIN", 'login');
+define("REGISTER", 'register');
+define("BILL", 'bill');
+define("ERROR", 'error');
+
 /**
  * JocolateManager
  * Web
@@ -14,87 +19,127 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @version 1.0.0
  * @see http://petritzdesigns.com
  */
-
 class Api extends CI_Controller {
+
+    private $error;
 
     public function __construct() {
         parent::__construct();
+        $this->error = "";
     }
-    
+
     public function index() {
         $this->load->template('welcome_message');
     }
-    
+
     public function login() {
         $this->load->library('Crypt');
         $crypt = new Crypt();
-        
+
         $this->load->library('Xml_writer');
         $xml = new Xml_writer;
         $xml->initiate();
 
-        $email = $crypt->decrypt($this->input->get('email', TRUE));
-        $password = $crypt->decrypt($this->input->get('pwd', TRUE));
-        if($this->user->login($email, $password)) {
-            //Success
-            $xml->addNode('login', 'Success');
-        } else {
-            //Failure
-            $xml->addNode('login', 'Failure');
+        try {
+            $email = $crypt->decrypt($this->get_input('email'));
+            $password = $crypt->decrypt($this->get_input('pwd'));
+
+            if ($this->user->login($email, $password)) {
+                //Success
+                $xml->addNode(LOGIN, 'Success');
+            } else {
+                //Failure
+                $xml->addNode(LOGIN, 'Failure');
+            }
+        } catch (Exception $exc) {
+            $xml->addNode(ERROR, $exc->getMessage());
         }
+
         $xml->getXml(true);
     }
-    
+
     public function register() {
         $this->load->library('Crypt');
         $crypt = new Crypt();
-        
-        $this->load->library('Xml_writer');
-        $xml = new Xml_writer;
-        $xml->initiate();
-        
-        $email = $crypt->decrypt($this->input->get('email', TRUE));
-        $password = $crypt->decrypt($this->input->get('pwd', TRUE));
-        if($this->user->register($email, $password)) {
-            //Success
-            $xml->addNode('register', 'Success');
-        } else {
-            //Failure
-            $xml->addNode('register', 'Failure');
-        }
-        $xml->getXml(true);
-    }
-    
-    public function incomingBill() {
-        $this->load->library('Crypt');
-        $crypt = new Crypt();
-        
+
         $this->load->library('Xml_writer');
         $xml = new Xml_writer;
         $xml->initiate();
 
-        $email = $crypt->decrypt($this->input->get('email', TRUE));
-        $password = $crypt->decrypt($this->input->get('pwd', TRUE));
-        $bill = $crypt->decrypt($this->input->get('bill', TRUE));
-        //store bill
-        $this->store_bill($bill);
-        if($this->user->login($email, $password)) {
-            //Success
-            $xml->addNode('bill', 'Success');
-        } else {
-            //Failure
-            $xml->addNode('login', 'Failure');
+        try {
+            $email = $crypt->decrypt($this->get_input('email'));
+            $password = $crypt->decrypt($this->get_input('pwd'));
+
+            if ($this->user->register($email, $password)) {
+                //Success
+                $xml->addNode(REGISTER, 'Success');
+            } else {
+                //Failure
+                $xml->addNode(REGISTER, 'Failure');
+            }
+        } catch (Exception $exc) {
+            $xml->addNode(ERROR, $exc->getMessage());
         }
+
         $xml->getXml(true);
     }
-    
+
+    public function incomingBill() {
+        $this->load->library('Crypt');
+        $crypt = new Crypt();
+
+        $this->load->library('Xml_writer');
+        $xml = new Xml_writer;
+        $xml->initiate();
+
+        try {
+            $email = $crypt->decrypt($this->get_input('email'));
+            $password = $crypt->decrypt($this->get_input('pwd'));
+            $bill = $crypt->decrypt($this->get_input('bill'));
+
+            if ($this->user->login($email, $password)) {
+                //Success
+                $this->store_bill($bill);
+                $xml->addNode(BILL, 'Success');
+            } else {
+                //Failure
+                $xml->addNode(BILL, 'Failure');
+            }
+        } catch (Exception $exc) {
+            $xml->addNode(ERROR, $exc->getMessage());
+        }
+        
+        $xml->getXml(true);
+    }
+
     public function logout() {
         $this->user->logout();
     }
-    
+
     private function store_bill($bill) {
         $file = fopen("bill.txt", "w");
         fwrite($file, $bill);
         fclose($file);
     }
+
+    private function get_input($name) {
+        if (empty($name)) {
+            throw new Exception("Empty input!");
+        }
+
+        //try GET first
+        $input_get = $this->input->get($name, TRUE);
+        if (isset($input_get)) {
+            return $input_get;
+        }
+
+        //then POST
+        $input_post = $this->input->post($name, TRUE);
+        if (isset($input_post)) {
+            return $input_get;
+        }
+
+        throw new Exception("Missing input: $name");
+    }
+
 }
