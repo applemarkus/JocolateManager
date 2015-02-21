ac9bb2d88c96618b895177e0d07ab9ac70d0d67e
petritzdesigns <petritzdesigns@gmail.com>
2015-02-21 11:31:18 +0100

    iPhone App First Commit

---
+++ iphone/JocolateManager/JocolateManager/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  JocolateManager
+//
+//  Created by Markus Petritz on 21.02.15.
+//  Copyright (c) 2015 PetritzDesigns. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
